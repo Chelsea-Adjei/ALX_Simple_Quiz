b3dba678eef06bdf3de4979f38c47ea1021e8a8b
Chelsea-Adjei <ruby.adjei02@gmail.com>
2024-10-05 14:14:50 +0000

    new updates

--- quiz.js
+++ quiz.js
@@ -1,20 +1,27 @@
 function checkAnswer(){
-    var correctAnswer = '4';
+    let correctAnswer = '4';
+    let userAnswer = document.querySelector('input[name="quiz"]:checked').value;
 
-    var userAnswer = document.querySelector('input[name="quiz"]:checked');
+    /*let answerValue = userAnswer.value;*/ // this can be used to run it.
 
-    if (userAnswer) {
-        var answerValue = userAnswer.value;
-    }
+    /*if (userAnswer) {   // this fomula also can be used to make it work.
+        answerValue = userAnswer.value
+        console.log( answerValue)
+    }*/
 
-    var feedbackElement = document.getElementById("feedback");
+
+
+    let feedbackElement = document.getElementById("feedback");
     if (userAnswer === correctAnswer) {
         feedbackElement.textContent = "Correct! Well done";
+        console.log(userAnswer, correctAnswer)
 
     } else {
         feedbackElement.textContent = "That's incorrect. Try again!";
+        console.log(userAnswer, correctAnswer)
     }
 }
 
 document.getElementById("submit-answer").addEventListener("click", checkAnswer);
+
 
